6ba8003abf4146f4b392683de891c3472f6ce35f
Fabricio Benitez y Alejandro Rincon <est.fabricio.benitez@unimilitar.edu.co>
2024-08-27 14:35:34 -0500

    Create pg1.java

---
+++ P1/pg1.java
@@ -0,0 +1,10 @@
+package com.mycompany.parcialpractica;
+
+class fraccion {
+    private int numerador, denominador, result;
+
+    public static void main(String[] args) {
+    get.(numerador);
+    get.(denominador);
+    }
+}
