370d08c5a6debdf284d54aa6fd479fed002cf0d4
Fabricio Benitez y Alejandro Rincon <est.fabricio.benitez@unimilitar.edu.co>
2024-08-27 14:51:23 -0500

    Create pg2.java

---
+++ P1/pg2.java
@@ -0,0 +1,13 @@
+package com.mycompany.parcialpractica;
+/*
+2. pregunta:con la misma clase de la pregunta anterior(fraccion),crear una sobrecarga del constructor y crear un objeto con cada una de sus formas
+*/
+public class fraccion {
+int mode,x,y;
+
+fraccion(){
+this.mode=1;
+this.x=1;
+this.y=1;
+}
+}
