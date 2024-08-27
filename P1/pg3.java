b486a45ada6f2b15daed6f111337df60d15b8b8c
Fabricio Benitez y Alejandro Rincon <est.fabricio.benitez@unimilitar.edu.co>
2024-08-27 15:02:02 -0500

    Create pg3.java

---
+++ P1/pg3.java
@@ -0,0 +1,14 @@
+package com.mycompany.parcialpractica;
+import java.lang.Math;
+/*
+3. pregunta:con la misma clase, crear un metodo pura, que me permita sumar 2 fracciones y que me devuelva su resultado
+*/
+public class fraccion {
+
+
+class static  metodo(int){
+    double n1,n2;
+    double result;
+    result= (n2+n1);
+};
+}
